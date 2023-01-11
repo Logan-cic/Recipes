ee6e1960eaf3582e16faae93717c49dbe6f871d5
Logan Cardoso <102470246+Logan-cic@users.noreply.github.com>
2023-01-11 08:12:05 -0300

    removing contact and about

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),  # Home
-    path('sobre/', sobre),  # dominioAnterior/sobre/
-    path('cotato/', contato),  # dominioAnterior/contato/
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')  # recipes é um namespece
-
-
-def contato(request):
-    return HttpResponse("contato")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
+    return render(request, 'recipes/home.html', context={
+        'name': 'Logan Cardoso'
+    })  # recipes é um namespece
